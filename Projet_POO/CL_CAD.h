ab0b5242a8445a7f3c37d431273098b35bc94e36
Tuturdh2414 <arthur.dharcourt@viacesi.fr>
2020-12-02 09:29:55 +0100

    version stock

---
+++ Projet_POO/CL_CAD.h
@@ -0,0 +1,29 @@
+#pragma once
+#using <System.data.dll>
+using namespace System::Data;
+using namespace System::Data::SqlClient;
+using namespace System::Xml;
+using namespace System;
+
+ref class CL_CAD
+{
+private:
+	SqlConnection^ connect;
+	SqlDataAdapter^ adapter;
+	DataSet^ DS;
+	//void setSQL(String^);
+public:
+	CL_CAD();
+	String^ select(int ligne, int colonne);
+	String^ selectWhere(String^ table, String^ entete, int ID, int colonne);
+	void ajouter(String^ table, String^ entete, String^ Value);
+	void action(String^ requeteSQL);
+	void supprimer(String^ table, String^ entete0, int ID);
+	void modifier(String^ table, String^ entete0, String^ entetemod, String^ mot, int ID);
+	int NB(String^ table);
+	int position(int ID);	//c'est la position dans la table (presque l'ID)
+	int verification(int ID, String^ table);
+	int MaxID();
+	DataSet^ getTableWhere(String^ table, String^ entete1, String^ entete0, int ID);
+
+};
--- Projet_POO/MyForm.h
+++ Projet_POO/MyForm.h
@@ -36,6 +36,14 @@ namespace ProjetPOO {
 		}
 	private: System::Windows::Forms::Button^ button1;
 	protected:
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::TextBox^ textBox6;
+
+	protected:
 
 	private:
 		/// <summary>
@@ -51,28 +59,87 @@ namespace ProjetPOO {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(90, 52);
+			this->button1->Location = System::Drawing::Point(328, 12);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"button1";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(312, 68);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 22);
+			this->textBox1->TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(312, 116);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 22);
+			this->textBox2->TabIndex = 2;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(312, 175);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 22);
+			this->textBox3->TabIndex = 3;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(312, 224);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(100, 22);
+			this->textBox4->TabIndex = 4;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(312, 280);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(100, 22);
+			this->textBox5->TabIndex = 5;
+			// 
+			// textBox6
+			// 
+			this->textBox6->Location = System::Drawing::Point(312, 340);
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(100, 22);
+			this->textBox6->TabIndex = 6;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(282, 253);
+			this->ClientSize = System::Drawing::Size(750, 469);
+			this->Controls->Add(this->textBox6);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	}
+};
 }
