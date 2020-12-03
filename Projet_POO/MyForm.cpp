80dc614dc459bd0d20187c8ed859ffd368728d1a
Tuturdh2414 <arthur.dharcourt@viacesi.fr>
2020-12-03 18:50:15 +0100

    version je sais pas ciombien

--- Projet_POO/MyForm.cpp
+++ Projet_POO/MyForm.cpp
@@ -14,7 +14,7 @@ int __clrcall WinMain(array<String^>^ args)
 
 void ProjetPOO::MyForm::afficher()
 {
-	/*Stock stocks(ligne);
+	Stock stocks(ligne);
 	
 	textBox1->Text = stocks.getreference();
 	textBox2->Text = stocks.getnom();
@@ -22,27 +22,28 @@ void ProjetPOO::MyForm::afficher()
 	textBox4->Text = stocks.getprix_HT();
 	textBox5->Text = stocks.getseuil_de_reaprovisionement();
 	textBox6->Text = stocks.gettaux_TVA();
-	*/
+	/*
 	Changement changement(ligne);
 	textBox7->Text = changement.getID();
 	textBox8->Text = changement.getdate();
 	textBox9->Text = changement.getreference();
-}
+	*/
+	}
 
 void ProjetPOO::MyForm::afficher(int reference, int ID)
 {
-	/*Stock stocks(ligne);
+	Stock stocks(ligne);
 	textBox1->Text = Convert::ToString(reference);
 	textBox2->Text = stocks.getnom(reference);
 	textBox3->Text = stocks.getquantite(reference);
 	textBox4->Text = stocks.getprix_HT(reference);
 	textBox5->Text = stocks.getseuil_de_reaprovisionement(reference);
 	textBox6->Text = stocks.gettaux_TVA(reference);
-	*/
+	/*
 	Changement changement(ligne);
 	textBox7->Text = Convert::ToString(ID);
 	textBox8->Text = changement.getdate(ID);
 	textBox9->Text = changement.getreference(ID);
-	
+	*/
 }
 
